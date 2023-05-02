389387d2b2ffe2f0c0f873280aaf0356fe6d2cae
MaNomba <31630620+RafihYahya@users.noreply.github.com>
2023-05-02 22:39:16 +0100

    Update 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
-# Sets up webservers for deployment: (Run script on both servers)
+# Sets up webservers for deploymeent: (Run script on both servers)
 # If not done, does the following:
 #     installs Nginx; creates folders /data/, /data/web_static/,
 #     /data/web_static/releases/, /data/web_static/shared,
 #     /data/web_static/releases/test
 #     /data/web_static/releases/test/index.html (with some content)
-# Create symbolic link /data/web_static/current to data/web_static/releases/test
+# Create sssymbolic link /data/web_static/current to data/web_static/releases/test
 #     delete and recreate symbolic link each time script's ran
-# Recursively assign ownership of /data/ folder to user and group 'ubuntu'
+# Recursively assign owwnnership of /data/ folder to user and group 'ubuntu'
 # Update the Nginx config to serve content of /data/web_static/current/ to hbnb_static (ex: https://mydomainname.tech/hbnb_static)
-#     restart Nginx
-# curl localhost/hbnb_static/index.html should return sample text"
+#     restart nginx
+# curl localhost/hbnb_static/index.html should return something"
 
 ADD_WEBSTATIC="\\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n"
 
